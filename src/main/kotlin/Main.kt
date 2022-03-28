84e964769d4e199e504ea9997533c8ae2e7ba0b3
AlieksandrDanil <inspcrimea@gmail.com>
2022-03-28 12:25:59 +0300

    Corrected fun 'agoToText' 'plural' according to coach notification

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,49 +1,42 @@
-const val SEC_NOW_FROM = 0
-const val SEC_NOW_TILL = 59
-const val SEC_MIN_FROM = 60
-const val SEC_MIN_TILL = 3599
-const val SEC_HOUR_FROM = 3600
-const val SEC_HOUR_TILL = 86399
-const val SEC_TODAY_FROM = 86400
-const val SEC_TODAY_TILL = 172799
-const val SEC_YESTERDAY_FROM = 172800
-const val SEC_YESTERDAY_TILL = 259199
+const val MINUTE = 60
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+const val TWO_DAYS = 2 * DAY
+const val THREE_DAYS = 3 * DAY
 
 fun main() {
     print("Введите целым числом кол-во секунд, когда абонент был в сети: ")
-    val number = readLine()?.toInt() ?: return
-        println("Собеседник был(а) в сети ${agoToText(number)}")
+    val sec = readLine()?.toInt() ?: return
+        println("Собеседник был(а) в сети ${agoToText(sec)}")
 }
 
-fun agoToText(sec: Int): String = when (sec) {
-    in SEC_NOW_FROM..SEC_NOW_TILL -> "только что"
-    in SEC_MIN_FROM..SEC_MIN_TILL -> {
-        val tempMin = sec / 60
-        plural(tempMin,true)
-    }
-    in SEC_HOUR_FROM..SEC_HOUR_TILL -> {
-        val tempHour = sec / 3600
-        plural(tempHour,false)
-    }
-    in SEC_TODAY_FROM..SEC_TODAY_TILL -> "сегодня"
-    in SEC_YESTERDAY_FROM..SEC_YESTERDAY_TILL -> "вчера"
-    else -> {
+fun agoToText(sec: Int): String {
+    return if (sec < MINUTE) "только что"
+    else if (sec < HOUR) {
+        val minutes = sec / MINUTE
+        val minutesPlural = plural(minutes, one = "минуту", few = "минуты", many = "минут")
+        "$minutes $minutesPlural назад"
+    } else if (sec < DAY) {
+        val hours = sec / HOUR
+        val hoursPlural = plural(hours, one = "час", few = "часа", many = "часов")
+        "$hours $hoursPlural назад"
+    } else if (sec < TWO_DAYS) {
+        "сегодня"
+    } else if (sec < THREE_DAYS) {
+        "вчера"
+    } else {
         "давно"
     }
 }
 
-fun plural(secToMinHour: Int, boolMinTrueHourFalse: Boolean): String {
-    return "${
-        when {
-            secToMinHour in 11..20 -> if (boolMinTrueHourFalse) "$secToMinHour минут"
-                else "$secToMinHour часов"
-            secToMinHour % 10 == 1 -> if (boolMinTrueHourFalse) "$secToMinHour минуту"
-                else "$secToMinHour час"
-            secToMinHour % 10 in 2..4 -> if (boolMinTrueHourFalse) "$secToMinHour минуты"
-                else "$secToMinHour часа"
-            else -> if (boolMinTrueHourFalse) "$secToMinHour минут"
-                else "$secToMinHour часов"
-        }
-    } назад"
+fun plural(amount: Int, one: String, few: String, many: String): String {
+    val reminder10 = amount % 10
+    val reminder100 = amount % 100
+    return when {
+        reminder100 in 11..20 -> many
+        reminder10 == 1 -> one
+        reminder10 in 2..4 -> few
+        else -> many
+    }
 }
 
